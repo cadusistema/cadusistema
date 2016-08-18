31b1b12199780d4e512a041d171c32641cca8000
erika.fidencio <henriquejgs@gmail.com>
2016-08-18 16:30:15 -0300

    parallalex em andamento

--- application/views/site.php
+++ application/views/site.php
@@ -4,9 +4,9 @@
     <header class="header" id="header"
     "><!--header-start-->
     <!--        <div class="container">-->
-    <!--            <figure class="logo animated fadeInDown delay-07s">-->
-    <!--                <a href="#"><img src="--><? // echo base_url(IMG_SITE.'logo.png') ?><!--" alt=""></a>-->
-    <!--            </figure>-->
+                <figure  >
+                    <a href="#"><img align="left" src="<? echo base_url(IMG_SITE.'pedratransparente.png') ?>" alt=""></a>
+                </figure>
     <!--            <h1 class="animated fadeInDown delay-07s">Welcome To Knight Studios</h1>-->
     <!--            <ul class="we-create animated fadeInUp delay-1s">-->
     <!--                <li>We are a digital agency that loves crafting beautiful websites.</li>-->
