2a119379a38b394374c4dab78d21e0d35edddbcb
erika.fidencio <henriquejgs@gmail.com>
2016-08-31 11:57:06 -0300

    ficha avaliação em andamento

--- application/layouts/js/admin/admin.js
+++ application/layouts/js/admin/admin.js
@@ -5,39 +5,23 @@ function validar(form) {
     return valida_required(form);
 }
 
-// function salvar() {
-//     if (validar()) {
-//         // var url = DOMAIN + "controller.php?page=salvar" + FUNC;
-//         // var dados = jQuery("#novo" + tipocliente).serializeArray();
-//         // $.post(
-//         //     url,
-//         //     dados,
-//         //     function (data) {
-//         //         eval("resp=" + data);
-//         //         if (resp.status > 0) {
-//         //             modal('teste', resp.msg, "sucesso");
-//         //             setTimeout(function () {
-//         //                 mudarPagina(tipocliente);
-//         //             }, DELAY);
-//         //
-//         //         } else {
-//         //             feedback('feedback', 'salvar');
-//         //             if (resp.status == 0)
-//         //                 modal('error_msg', resp.msg, 'alerta', 1);
-//         //             else
-//         //                 modal('error_msg', resp.msg, 'alerta', 1);
-//         //         }
-//         //
-//         //     }
-//         // );
-//
-//         alert("if-validar");
-//     }
-//     else {
-//         // feedback('feedback', 'salvar');
-//         alert('else-validar');
-//     }
-//
-//
-//
-// }
+function salvar(form) {
+    if (validar(form)) {
+        var dados = jQuery("#" + form).serializeArray();
+        // var url = '<?php echo base_url(Autorizacao/salvaAutorizacaoDB);?>';
+
+        console.log(dados);
+        // console.log(url);
+
+
+
+
+        // alert("if-validar");
+    }
+    else {
+        // feedback('feedback', 'salvar');
+        // alert('else-validar');
+    }
+
+
+}
