599d85a0960df62467f9acd6b8336aa573aea916
erika.fidencio <henriquejgs@gmail.com>
2016-09-01 11:18:47 -0300

    ficha de autorização em andamento

--- application/layouts/js/admin/admin.js
+++ application/layouts/js/admin/admin.js
@@ -26,6 +26,37 @@ function salvar(form) {
         // feedback('feedback', 'salvar');
         alert('else-validar');
     }
+}
 
+function addMembros() {
+    // console.log("Membros");
+    $("#teste").append(createDivFields());
+    return false;
 
 }
+
+function createDivFields() {
+    /*
+     Criamos a variavel, e atribuimos os campos que serão criados;
+     Utilizamos o colchetes nos nomes do campos para informar que os dados
+     em forma de array;
+     Adiciona uma div, para que nela seja criado novos campos extras;
+     E um link para para chamar o evento de adicionar;
+     */
+    var html = '<div class="field form-group novomembro">';
+    html += '<label>Nome do Integrante</label>    <input type="text" name="namemember[]" value="" id="namemember" class="form-control input-sm"  />';
+    html += '<label>Instituição do Integrante</label>    <input type="text" name="institutionmember[]" value="" id="institutionmember" class="form-control input-sm"  />';
+    html += '<label>CPF / RG</label>    <input type="text" name="cpfrg[]" value="" id="cpfrg" class="form-control input-sm"  />';
+    html += '<a href="#" id="buttonremovemembro" onclick="removemembro()">Remover Membro</a>';
+    html += '<div class="item"></div>';
+    html += '<div>';
+    return html;
+}
+
+function removemembro () {
+    console.log("etateteta");
+    $(".novomembro").remove();
+}
+
+
+
--- application/views/admin/autorizacaoparapesquisa.php
+++ application/views/admin/autorizacaoparapesquisa.php
@@ -23,214 +23,214 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         'required '   => 'required',
         'placeholder' => 'Titulo'
     )); ?>
-<!--</div>-->
-<!--<div class="field form-group">-->
-<!--    --><?//
-//    echo form_label("Instituição*", 'institution'); ?>
-<!--    --><?// echo form_input(array(
-//        "id"          => "institution",
-//        "name"        => "institution",
-//        "class"       => "form-control input-sm",
-//        "maxlength"   => "255",
-//        "onfocus"     => 'remove_erro_validacao(this.id)',
-//        'placeholder' => "Instituição",
-//        'required '   => 'required',
-//    )); ?>
-<!--</div>-->
-<!--<div class="field form-group">-->
-<!--    --><?//
-//    echo form_label("Responsável*", 'responsible'); ?>
-<!--    --><?// echo form_input(array(
-//        "id"          => "responsible",
-//        "name"        => "responsible",
-//        "class"       => "form-control input-sm",
-//        "onfocus"     => 'remove_erro_validacao(this.id)',
-//        'placeholder' => 'Responsável',
-//        'required '   => 'required',
-//    ));
-//    ?>
-<!--</div>-->
-<!--<div class="field form-group">-->
-<!--    --><?//
-//    echo form_label("CPF*", 'cpf'); ?>
-<!--    --><?// echo form_input(array(
-//        "id"          => "cpf",
-//        "name"        => "cpf",
-//        "class"       => "form-control input-sm",
-//        "onfocus"     => 'remove_erro_validacao(this.id)',
-//        'placeholder' => "CPF",
-//        'required '   => 'required',
-//    ));
-//    ?>
-<!--</div>-->
-<!--<div class="field form-group">-->
-<!--    --><?//
-//    echo form_label("Endereço*", 'address'); ?>
-<!--    --><?// echo form_input(array(
-//        "id"          => "address",
-//        "name"        => "address",
-//        "class"       => "form-control input-sm",
-//        'placeholder' => 'Endereço',
-//        "onfocus"     => 'remove_erro_validacao(this.id)',
-//        'required '   => 'required',
-//    ));
-//    ?>
-<!--</div>-->
-<!--<div class="field form-group">-->
-<!--    --><?//
-//    echo form_label("Número / Km"); ?>
-<!--    --><?// echo form_input(array(
-//        "id"          => "number",
-//        "name"        => "number",
-//        "class"       => "form-control input-sm",
-//        'placeholder' => 'Número / Km',
-//    ));
-//    ?>
-<!--</div>-->
-<!--<div class="field form-group">-->
-<!--    --><?//
-//    echo form_label("Bairro*", 'neighborhood'); ?>
-<!--    --><?// echo form_input(array(
-//        "id"          => "neighborhood",
-//        "name"        => "neighborhood",
-//        "class"       => "form-control input-sm",
-//        'placeholder' => 'Bairro',
-//        "onfocus"     => 'remove_erro_validacao(this.id)',
-//        'required '   => 'required',
-//    ));
-//    ?>
-<!--</div>-->
-<!--<div class="field form-group">-->
-<!--    --><?//
-//    echo form_label("Complemento"); ?>
-<!--    --><?// echo form_input(array(
-//        "id"          => "complement",
-//        "name"        => "complement",
-//        "class"       => "form-control input-sm",
-//        'placeholder' => 'Complemento',
-//    ));
-//    ?>
-<!--</div>-->
-<!--<div class="field form-group">-->
-<!--    --><?//
-//    echo form_label("Município*", 'city'); ?>
-<!--    --><?// echo form_input(array(
-//        "id"          => "city",
-//        "name"        => "city",
-//        "class"       => "form-control input-sm",
-//        'placeholder' => 'Cidade',
-//        'required '   => 'required',
-//        "onfocus"     => 'remove_erro_validacao(this.id)'
-//    ));
-//    ?>
-<!--</div>-->
-<!--<div class="field form-group">-->
-<!--    --><?//
-//    //confirmar dispois
-//    echo form_label("UF*", 'uf'); ?>
-<!--    --><?// echo form_input(array(
-//        "id"          => "uf",
-//        "name"        => "uf",
-//        "class"       => "form-control input-sm",
-//        "onfocus"     => 'remove_erro_validacao(this.id)',
-//        'placeholder' => 'UF',
-//        'required '   => 'required',
-//    ));
-//    ?>
-<!--</div>-->
-<!--<div class="field form-group">-->
-<!--    --><?//
-//    echo form_label("CEP*", 'zipecode'); ?>
-<!--    --><?// echo form_input(array(
-//        "id"          => "zipecode",
-//        "name"        => "zipecode",
-//        "class"       => "form-control input-sm",
-//        "onfocus"     => 'remove_erro_validacao(this.id)',
-//        'placeholder' => 'CEP',
-//        'required '   => 'required',
-//    ));
-//    ?>
-<!--</div>-->
-<!--<div class="field form-group">-->
-<!--    --><?//
-//    echo form_label("Telefone*:", 'phone'); ?>
-<!--    --><?// echo form_input(array(
-//        "id"          => "phone",
-//        "name"        => "phone",
-//        "class"       => "form-control input-sm",
-//        'placeholder' => 'Telefone',
-//        "onfocus"     => 'remove_erro_validacao(this.id)',
-//        'required '   => 'required',
-//    ));
-//    ?>
-<!--</div>-->
-<!--<div class="field form-group">-->
-<!--    --><?//
-//    echo form_label("Celular:", 'cellphone'); ?>
-<!--    --><?// echo form_input(array(
-//        "id"          => "cellphone",
-//        "name"        => "cellphone",
-//        "class"       => "form-control input-sm",
-//        'placeholder' => 'Endereço',
-//        "onfocus"     => 'remove_erro_validacao(this.id)',
-//        'required '   => 'required',
-//    ));
-//    ?>
-<!--</div>-->
-<!--<div class="field form-group">-->
-<!--    --><?//
-//    echo form_label("E-mail*:", 'email'); ?>
-<!--    --><?// echo form_input(array(
-//        "id"          => "email",
-//        "name"        => "email",
-//        "class"       => "form-control input-sm",
-//        'placeholder' => 'Email',
-//        "onfocus"     => 'remove_erro_validacao(this.id)',
-//        'required '   => 'required',
-//    ));
-//    ?>
-<!--</div>-->
-<!--<div class="field form-group">-->
-<!--    --><?//
-//    echo form_label("Nome do Integrante"); ?>
-<!--    --><?// echo form_input(array(
-//        "id"    => "namemember",
-//        "name"  => "namemember",
-//        "class" => "form-control input-sm",
-//    ));
-//    ?>
-<!--</div>-->
-<!--<div class="field form-group">-->
-<!--    --><?//
-//    echo form_label("Instituição do Integrante"); ?>
-<!--    --><?// echo form_input(array(
-//        "id"    => "institutionmember",
-//        "name"  => "institutionmember",
-//        "class" => "form-control input-sm",
-//    ));
-//    ?>
-<!--</div>-->
-<!--<div class="field form-group">-->
-<!--    --><?//
-//    echo form_label("CPF / RG"); ?>
-<!--    --><?// echo form_input(array(
-//        "id"    => "cpfrg",
-//        "name"  => "cpfrg",
-//        "class" => "form-control input-sm",
-//    ));
-//    ?>
-<!--</div>-->
-<!--<div class="field form-group">-->
-<!--    --><?//
-//    echo form_label("Função do Integrante"); ?>
-<!--    --><?// echo form_input(array(
-//        "id"    => "functionmember",
-//        "name"  => "functionmember",
-//        "class" => "form-control input-sm",
-//    ));
-//    ?>
-<!--</div>-->
-<?//
+</div>
+<div class="field form-group">
+    <?
+    echo form_label("Instituição*", 'institution'); ?>
+    <? echo form_input(array(
+        "id"          => "institution",
+        "name"        => "institution",
+        "class"       => "form-control input-sm",
+        "maxlength"   => "255",
+        "onfocus"     => 'remove_erro_validacao(this.id)',
+        'placeholder' => "Instituição",
+        'required '   => 'required',
+    )); ?>
+</div>
+<div class="field form-group">
+    <?
+    echo form_label("Responsável*", 'responsible'); ?>
+    <? echo form_input(array(
+        "id"          => "responsible",
+        "name"        => "responsible",
+        "class"       => "form-control input-sm",
+        "onfocus"     => 'remove_erro_validacao(this.id)',
+        'placeholder' => 'Responsável',
+        'required '   => 'required',
+    ));
+    ?>
+</div>
+<div class="field form-group">
+    <?
+    echo form_label("CPF*", 'cpf'); ?>
+    <? echo form_input(array(
+        "id"          => "cpf",
+        "name"        => "cpf",
+        "class"       => "form-control input-sm",
+        "onfocus"     => 'remove_erro_validacao(this.id)',
+        'placeholder' => "CPF",
+        'required '   => 'required',
+    ));
+    ?>
+</div>
+<div class="field form-group">
+    <?
+    echo form_label("Endereço*", 'address'); ?>
+    <? echo form_input(array(
+        "id"          => "address",
+        "name"        => "address",
+        "class"       => "form-control input-sm",
+        'placeholder' => 'Endereço',
+        "onfocus"     => 'remove_erro_validacao(this.id)',
+        'required '   => 'required',
+    ));
+    ?>
+</div>
+<div class="field form-group">
+    <?
+    echo form_label("Número / Km"); ?>
+    <? echo form_input(array(
+        "id"          => "number",
+        "name"        => "number",
+        "class"       => "form-control input-sm",
+        'placeholder' => 'Número / Km',
+    ));
+    ?>
+</div>
+<div class="field form-group">
+    <?
+    echo form_label("Bairro*", 'neighborhood'); ?>
+    <? echo form_input(array(
+        "id"          => "neighborhood",
+        "name"        => "neighborhood",
+        "class"       => "form-control input-sm",
+        'placeholder' => 'Bairro',
+        "onfocus"     => 'remove_erro_validacao(this.id)',
+        'required '   => 'required',
+    ));
+    ?>
+</div>
+<div class="field form-group">
+    <?
+    echo form_label("Complemento"); ?>
+    <? echo form_input(array(
+        "id"          => "complement",
+        "name"        => "complement",
+        "class"       => "form-control input-sm",
+        'placeholder' => 'Complemento',
+    ));
+    ?>
+</div>
+<div class="field form-group">
+    <?
+    echo form_label("Município*", 'city'); ?>
+    <? echo form_input(array(
+        "id"          => "city",
+        "name"        => "city",
+        "class"       => "form-control input-sm",
+        'placeholder' => 'Cidade',
+        'required '   => 'required',
+        "onfocus"     => 'remove_erro_validacao(this.id)'
+    ));
+    ?>
+</div>
+<div class="field form-group">
+    <?
+    //confirmar dispois
+    echo form_label("UF*", 'uf'); ?>
+    <? echo form_input(array(
+        "id"          => "uf",
+        "name"        => "uf",
+        "class"       => "form-control input-sm",
+        "onfocus"     => 'remove_erro_validacao(this.id)',
+        'placeholder' => 'UF',
+        'required '   => 'required',
+    ));
+    ?>
+</div>
+<div class="field form-group">
+    <?
+    echo form_label("CEP*", 'zipecode'); ?>
+    <? echo form_input(array(
+        "id"          => "zipecode",
+        "name"        => "zipecode",
+        "class"       => "form-control input-sm",
+        "onfocus"     => 'remove_erro_validacao(this.id)',
+        'placeholder' => 'CEP',
+        'required '   => 'required',
+    ));
+    ?>
+</div>
+<div class="field form-group">
+    <?
+    echo form_label("Telefone*:", 'phone'); ?>
+    <? echo form_input(array(
+        "id"          => "phone",
+        "name"        => "phone",
+        "class"       => "form-control input-sm",
+        'placeholder' => 'Telefone',
+        "onfocus"     => 'remove_erro_validacao(this.id)',
+        'required '   => 'required',
+    ));
+    ?>
+</div>
+<div class="field form-group">
+    <?
+    echo form_label("Celular:", 'cellphone'); ?>
+    <? echo form_input(array(
+        "id"          => "cellphone",
+        "name"        => "cellphone",
+        "class"       => "form-control input-sm",
+        'placeholder' => 'Endereço',
+        "onfocus"     => 'remove_erro_validacao(this.id)',
+        'required '   => 'required',
+    ));
+    ?>
+</div>
+<div class="field form-group">
+    <?
+    echo form_label("E-mail*:", 'email'); ?>
+    <? echo form_input(array(
+        "id"          => "email",
+        "name"        => "email",
+        "class"       => "form-control input-sm",
+        'placeholder' => 'Email',
+        "onfocus"     => 'remove_erro_validacao(this.id)',
+        'required '   => 'required',
+    ));
+    ?>
+</div>
+<div class="field form-group">
+    <?
+    echo form_label("Nome do Integrante"); ?>
+    <? echo form_input(array(
+        "id"    => "namemember",
+        "name"  => "namemember[]",
+        "class" => "form-control input-sm",
+    ));
+    ?>
+</div>
+<div class="field form-group">
+    <?
+    echo form_label("Instituição do Integrante"); ?>
+    <? echo form_input(array(
+        "id"    => "institutionmember",
+        "name"  => "institutionmember[]",
+        "class" => "form-control input-sm",
+    ));
+    ?>
+</div>
+<div class="field form-group">
+    <?
+    echo form_label("CPF / RG"); ?>
+    <? echo form_input(array(
+        "id"    => "cpfrg",
+        "name"  => "cpfrg[]",
+        "class" => "form-control input-sm",
+    ));
+    ?>
+</div>
+<div class="field form-group" id="teste">
+    <?
+    echo form_label("Função do Integrante"); ?>
+    <? echo form_input(array(
+        "id"    => "functionmember",
+        "name"  => "functionmember[]",
+        "class" => "form-control input-sm",
+    ));
+    ?>
+</div>
+<?
 //echo form_submit(array(
 //    'id'      => "submit",
 //    'name'    => "salvaautorizacao",
@@ -247,6 +247,7 @@ echo form_close();
 //    'onclick' => "validar('formautorizacao')"
 //));
 ?>
+<a href="javascript:void(0)" class="btn btn-primary" id="salvar" onclick="addMembros()"> Adicionar Membros</a>
 <a href="javascript:void(0)" class="btn btn-primary" id="salvar" onclick="salvar('formautorizacao')"><i
         class="fa fa-floppy-o"> Salvar</i></a>
 <a href="javascript:history.go(-1)" class="btn btn-default" title="Cancelar"><i class="fa fa-arrow-left"></i>
