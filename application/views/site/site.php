4fa6cb1f1a8ae0f7d8defa2ef3e9a0c1f9a35fc5
erika.fidencio <henriquejgs@gmail.com>
2016-10-03 12:05:38 -0300

    comentando parte do 'atividades desenvolvidas'

--- application/views/site/site.php
+++ application/views/site/site.php
@@ -192,37 +192,37 @@
     <div class="container">
         <h2>Atividades Desenvolvidas</h2>
         <h6>O parque oferece.</h6>
-        <div class="row">
-            <div class="col-lg-7 col-sm-6 wow fadeInRight delay-02s" style="float: right">
-                <div class="service-list">
-                    <div class="service-list-col1">
-                        <img src="<? echo base_url(ICONE . 'administracao.ico') ?>" height="55px"/>
-                    </div>
-                    <div class="service-list-col2">
-                        <h3>Centro Administrativo</h3>
-                        <p>Centro Administrativo conta com dois vestiários.</p>
-                    </div>
-                </div>
-                <div class="service-list">
-                    <div class="service-list-col1">
-                        <img src="<? echo base_url(ICONE . 'areasports.ico') ?>"/>
-                    </div>
-                    <div class="service-list-col2">
-                        <h3>Área de Lazer</h3>
-                        <p>1 campo de futebol,1quadra de areia,1 quadra poliesportiva .</p>
-                    </div>
-                </div>
-                <div class="service-list">
-                    <div class="service-list-col1">
-                        <img src="<? echo base_url(ICONE . 'ordonance.ico') ?>"/>
-                    </div>
-                    <div class="service-list-col2">
-                        <h3>Portaria</h3>
-                        <p>Proin iaculis purus consequat digni sem digni ssim. Purus donec porttitora entum.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+<!--        <div class="row">-->
+<!--            <div class="col-lg-7 col-sm-6 wow fadeInRight delay-02s" style="float: right">-->
+<!--                <div class="service-list">-->
+<!--                    <div class="service-list-col1">-->
+<!--                        <img src="--><?// echo base_url(ICONE . 'administracao.ico') ?><!--" height="55px"/>-->
+<!--                    </div>-->
+<!--                    <div class="service-list-col2">-->
+<!--                        <h3>Centro Administrativo</h3>-->
+<!--                        <p>Centro Administrativo conta com dois vestiários.</p>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <div class="service-list">-->
+<!--                    <div class="service-list-col1">-->
+<!--                        <img src="--><?// echo base_url(ICONE . 'areasports.ico') ?><!--"/>-->
+<!--                    </div>-->
+<!--                    <div class="service-list-col2">-->
+<!--                        <h3>Área de Lazer</h3>-->
+<!--                        <p>1 campo de futebol,1quadra de areia,1 quadra poliesportiva .</p>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <div class="service-list">-->
+<!--                    <div class="service-list-col1">-->
+<!--                        <img src="--><?// echo base_url(ICONE . 'ordonance.ico') ?><!--"/>-->
+<!--                    </div>-->
+<!--                    <div class="service-list-col2">-->
+<!--                        <h3>Portaria</h3>-->
+<!--                        <p>Proin iaculis purus consequat digni sem digni ssim. Purus donec porttitora entum.</p>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
 
     </div>
 </section><!--main-section-end-->
