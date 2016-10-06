14ea5d042fb5fc9fe79c8d8264877bea09f3fe2b
erika.fidencio <henriquejgs@gmail.com>
2016-10-06 11:56:04 -0300

    atrativos em andamento

--- application/views/site/atrativos.php
+++ application/views/site/atrativos.php
@@ -5,142 +5,156 @@
  * Date: 23/09/2016
  * Time: 15:16
  */ ?>
-<h2>Cachoeiras</h2>
-
-<section>
-    <div>
-        <h3>Cachoeira das Andorinhas</h3>
-        <figure>
-            <img src="<? echo base_url() . IMG_ATRATIVOS ?>cachoeiraandorinhas.jpg" alt="cachoeira das andorinhas">
-        </figure>
-        <div>
-            A Cachoeira das Andorinhas possui uma beleza natural singular, conta com uma queda d'água de cerca de 10
-            metros de altura, situada no interior de uma formação rochosoa que se assemelha a uma gruta. É assim chamada
-            por abrigar uma considerável quantidade de andorinhas de coleira durante a primavera-verão.
+<section class="atrativos">
+    <h2>Cachoeiras</h2>
+    <div class="container">
+        <div >
+            <h3>Cachoeira das Andorinhas</h3>
+            <figure>
+                <img src="<? echo base_url() . IMG_ATRATIVOS ?>cachoeiraandorinhas.jpg" alt="cachoeira das andorinhas">
+            </figure>
+            <div >
+                A Cachoeira das Andorinhas possui uma beleza natural singular, conta com uma queda d'água de cerca de 10
+                metros de altura, situada no interior de uma formação rochosoa que se assemelha a uma gruta. É assim
+                chamada
+                por abrigar uma considerável quantidade de andorinhas de coleira durante a primavera-verão.
+            </div>
+            <div >
+                <strong>Distância da sede do parque</strong>: 550m.<br>
+                <strong>Tempo de percurso</strong>: 20 minutos. <br>
+                <strong>Nível de dificuldade de acesso</strong>: normal. <br>
+            </div>
         </div>
         <div>
-            Distância da sede do parque: 550m.<br>
-            Tempo de percurso: 20 minutos. <br>
-            Nível de dificuldade de acesso: normal. <br>
-        </div>
-    </div>
-    <div>
-        <h3>Cachoeira dos Pelados</h3>
-        <figure>
-            <img src="<? echo base_url() . IMG_ATRATIVOS ?>cachoeiradospelados.jpg" alt="cachoeira dos pelados">
-        </figure>
-        <div>
-            A Cachoeira dos Pelados situa-se a cerca de 600 metros da sede do parque. Esta cachoeira, de beleza cênica
-            exuberante, conta com uma queda d'água de aproximadamente 5 metros de altura, perfeita para se refrescar em
-            ambiente tranquilo e harmonioso.
-        </div>
-        <div>
-            Distância da sede do parque: 600m.<br>
-            Tempo de percurso: 40 minutos. <br>
-            Nível de dificuldade de acesso: dificil. <br>
-        </div>
-    </div>
-    <div>
-        <h3>Cachoeira Véu das Noivas</h3>
-        <figure>
-            <img src="<? echo base_url() . IMG_ATRATIVOS ?>cachoeiraveudanoiva.jpg" alt="cachoeira Veu das Noivas">
-        </figure>
-        <div>
-            A Cachoeira Véu das Noivas apresenta duas quedas d'água que somam cerca de 40 metros de altura de
-            deslumbrante e imponente beleza natural. Chegar a base desta cachoeira é tarefa para os visitantes mais
-            aventureiros, pois é necessário atravessar uma trilha bem fechada e muito íngrime que liga o topo à base
-            desta cachoeira.
+            <h3>Cachoeira dos Pelados</h3>
+            <figure>
+                <img src="<? echo base_url() . IMG_ATRATIVOS ?>cachoeiradospelados.jpg" alt="cachoeira dos pelados">
+            </figure>
+            <div >
+                A Cachoeira dos Pelados situa-se a cerca de 600 metros da sede do parque. Esta cachoeira, de beleza
+                cênica
+                exuberante, conta com uma queda d'água de aproximadamente 5 metros de altura, perfeita para se refrescar
+                em
+                ambiente tranquilo e harmonioso.
+            </div>
+            <div >
+                <strong>Distância da sede do parque</strong>: 600 m.<br>
+                <strong>Tempo de percurso</strong>: 40 minutos. <br>
+                <strong>Nível de dificuldade de acesso</strong>: dificil. <br>
+            </div>
         </div>
         <div>
-            Distância da sede do parque: 1,10 m.<br>
-            Tempo de percurso: 60 minutos. <br>
-            Nível de dificuldade de acesso: muito dificil. <br>
-        </div>
-    </div>
-    <div>
-        <h3>Cachoeira da Folhinha</h3>
-        <figure>
-            <img src="<? echo base_url() . IMG_ATRATIVOS ?>cachoeiradafolhinha.jpg" alt="cachoeirada Folhinha">
-        </figure>
-        <div>
-            A Cachoeira da Folhinha é um dos pontos mais procurados pelos visitantes que moram na região. Este atrativo
-            turístico natural de beleza magnífica, conta com um grande poço que se assemelha a um belo lago, ideal para
-            nadar e relaxar. Esta cachoeira é chamada assim devido a grande quantidade de vegetação contendo pequenas
-            folhas que margeam o local, engrandecendo ainda mais seu fascinante encanto natural.
-        </div>
-        <div>
-            Distância da sede do parque: 2,45 km.<br>
-            Tempo de percurso: 75 minutos. <br>
-            Nível de dificuldade de acesso: muito dificil. <br>
-        </div>
-    </div>
-    <div>
-        <h3>Praia do Rancheiro</h3>
-        <figure>
-            <img src="<? echo base_url() . IMG_ATRATIVOS ?>praiadorancheiro.jpg" alt="Praia do Rancheiro ">
-        </figure>
-        <div>
-            A Praia do Rancheiro é um excelente local para nadar e revigorar as energias em boa companhia. Contêm um
-            grande poço de pouco mais de 1,5 metros de profundidade, além de uma extensa área plana coberta por
-            gramínias. Ótimo local para se acampar e fazer piquiniques com amigos e família. Lembrando que não é
-            permitido fazer fogueiras nas dependências do parque. Evite acampar sozinho para melhor garantir sua própria
-            segurança.
+            <h3>Cachoeira Véu das Noivas</h3>
+            <figure>
+                <img src="<? echo base_url() . IMG_ATRATIVOS ?>cachoeiraveudanoiva.jpg" alt="cachoeira Veu das Noivas">
+            </figure>
+            <div >
+                A Cachoeira Véu das Noivas apresenta duas quedas d'água que somam cerca de 40 metros de altura de
+                deslumbrante e imponente beleza natural. Chegar a base desta cachoeira é tarefa para os visitantes mais
+                aventureiros, pois é necessário atravessar uma trilha bem fechada e muito íngrime que liga o topo à base
+                desta cachoeira.
+            </div>
+            <div >
+                <strong>Distância da sede do parque</strong>: 1,10 m.<br>
+                <strong>Tempo de percurso</strong>: 60 minutos. <br>
+                <strong>Nível de dificuldade de acesso</strong>: muito dificil. <br>
+            </div>
         </div>
         <div>
-            Distância da sede do parque: 3,0 km.<br>
-            Tempo de percurso: 100 minutos. <br>
-            Nível de dificuldade de acesso: muito dificil. <br>
-        </div>
-    </div>
-    <div>
-        <h3>Cachoeira do Carioquinha</h3>
-        <figure>
-            <img src="<? echo base_url() . IMG_ATRATIVOS ?>cachoeiradocarioquinha.jpg" alt="Cachoeira do Carioquinha">
-        </figure>
-        <div>
-            A Cachoeira do Carioquinha é de extrema beleza cênica, contendo uma pequena e refrescante queda d'água, além
-            de um extenso poço, onde a profundidade aumenta progressivamente, chegando até cerca de 1,5 metros de
-            profundidade próximo da queda d'água. É ideal para visitantes que tem receio de nadar em cachoeiras mais
-            profundas e com grandes quedas d'água.
-        </div>
-        <div>
-            Distância da sede do parque: 1,54 km.<br>
-            Tempo de percurso: 60 minutos. <br>
-            Nível de dificuldade de acesso: normal. <br>
-        </div>
-    </div>
-    <div>
-        <h3>Pocinho do Baú</h3>
-        <figure>
-            <img src="<? echo base_url() . IMG_ATRATIVOS ?>pocinhodobau.jpg" alt="Pocinho do Baú">
-        </figure>
-        <div>
-            O Pocinho do Baú é ideal para visitantes que não desejam caminhar longos percursos para desfrutar de uma
-            área para nadar com espetacular beleza natural. Ese poço apresenta cerca de 1,80 metros de profundidade, em
-            águas calmas sem correnteza, além de uma grande área plana coberta por gramíneas. Perfeito local para
-            piquiniques e brincadeiras para crianças. Lembrando que não é permitido fazer fogueiras nas dependências do
-            parque.
+            <h3>Cachoeira da Folhinha</h3>
+            <figure>
+                <img src="<? echo base_url() . IMG_ATRATIVOS ?>cachoeiradafolhinha.jpg" alt="cachoeirada Folhinha">
+            </figure>
+            <div >
+                A Cachoeira da Folhinha é um dos pontos mais procurados pelos visitantes que moram na região. Este
+                atrativo
+                turístico natural de beleza magnífica, conta com um grande poço que se assemelha a um belo lago, ideal
+                para
+                nadar e relaxar. Esta cachoeira é chamada assim devido a grande quantidade de vegetação contendo
+                pequenas
+                folhas que margeam o local, engrandecendo ainda mais seu fascinante encanto natural.
+            </div>
+            <div >
+                <strong>Distância da sede do parque</strong>: 2,45 km.<br>
+                <strong> Tempo de percurso</strong>: 75 minutos. <br>
+                <strong>Nível de dificuldade de acesso</strong>: muito dificil. <br>
+            </div>
         </div>
         <div>
-            Distância da sede do parque: 690 m.<br>
-            Tempo de percurso: 15 minutos. <br>
-            Nível de dificuldade de acesso: fácil. <br>
+            <h3>Praia do Rancheiro</h3>
+            <figure>
+                <img src="<? echo base_url() . IMG_ATRATIVOS ?>praiadorancheiro.jpg" alt="Praia do Rancheiro ">
+            </figure>
+            <div >
+                A Praia do Rancheiro é um excelente local para nadar e revigorar as energias em boa companhia. Contêm um
+                grande poço de pouco mais de 1,5 metros de profundidade, além de uma extensa área plana coberta por
+                gramínias. Ótimo local para se acampar e fazer piquiniques com amigos e família. Lembrando que não é
+                permitido fazer fogueiras nas dependências do parque. Evite acampar sozinho para melhor garantir sua
+                própria
+                segurança.
+            </div>
+            <div >
+                <strong>Distância da sede do parque</strong>: 3,0 km.<br>
+                <strong>Tempo de percurso</strong>: 100 minutos. <br>
+                <strong>Nível de dificuldade de acesso</strong>: muito dificil. <br>
+            </div>
         </div>
-    </div>
-    <div>
-        <h3>Pocinho das Churrasqueiras</h3>
-        <figure>
-            <img src="<? echo base_url() . IMG_ATRATIVOS ?>pocinhodaschurrasqueiras.jpg" alt="Pocinho das Churrasqueiras">
-        </figure>
         <div>
-            O Pocinho das Churrasqueiras é de extremo encanto natural que se encontra próximo a área de churrasqueiras.
-            Poço de cerca de 1,0 metros de profundidade, ótimo para se refrescar com as crianças, família e amigos
-            enquanto prepara um saboroso churrasco nos dias ensolarados.
+            <h3>Cachoeira do Carioquinha</h3>
+            <figure>
+                <img src="<? echo base_url() . IMG_ATRATIVOS ?>cachoeiradocarioquinha.jpg"
+                     alt="Cachoeira do Carioquinha">
+            </figure>
+            <div >
+                A Cachoeira do Carioquinha é de extrema beleza cênica, contendo uma pequena e refrescante queda d'água,
+                além
+                de um extenso poço, onde a profundidade aumenta progressivamente, chegando até cerca de 1,5 metros de
+                profundidade próximo da queda d'água. É ideal para visitantes que tem receio de nadar em cachoeiras mais
+                profundas e com grandes quedas d'água.
+            </div>
+            <div >
+                <strong>Distância da sede do parque</strong>: 1,54 km.<br>
+                <strong>Tempo de percurso</strong>: 60 minutos. <br>
+                <strong>Nível de dificuldade de acesso</strong>: normal. <br>
+            </div>
         </div>
         <div>
-            Distância da sede do parque: 170 m.<br>
-            Tempo de percurso: 3 minutos. <br>
-            Nível de dificuldade de acesso: muito fácil. <br>
+            <h3>Pocinho do Baú</h3>
+            <figure>
+                <img src="<? echo base_url() . IMG_ATRATIVOS ?>pocinhodobau.jpg" alt="Pocinho do Baú">
+            </figure>
+            <div >
+                O Pocinho do Baú é ideal para visitantes que não desejam caminhar longos percursos para desfrutar de uma
+                área para nadar com espetacular beleza natural. Ese poço apresenta cerca de 1,80 metros de profundidade,
+                em
+                águas calmas sem correnteza, além de uma grande área plana coberta por gramíneas. Perfeito local para
+                piquiniques e brincadeiras para crianças. Lembrando que não é permitido fazer fogueiras nas dependências
+                do
+                parque.
+            </div>
+            <div >
+                <strong>Distância da sede do parque</strong>: 690 m.<br>
+                <strong>Tempo de percurso</strong>: 15 minutos. <br>
+                <strong>Nível de dificuldade de acesso</strong>: fácil. <br>
+            </div>
+        </div>
+        <div>
+            <h3>Pocinho das Churrasqueiras</h3>
+            <figure>
+                <img src="<? echo base_url() . IMG_ATRATIVOS ?>pocinhodaschurrasqueiras.jpg"
+                     alt="Pocinho das Churrasqueiras">
+            </figure>
+            <div >
+                O Pocinho das Churrasqueiras é de extremo encanto natural que se encontra próximo a área de
+                churrasqueiras.
+                Poço de cerca de 1,0 metros de profundidade, ótimo para se refrescar com as crianças, família e amigos
+                enquanto prepara um saboroso churrasco nos dias ensolarados.
+            </div>
+            <div >
+                <strong>Distância da sede do parque</strong>: 170 m.<br>
+                <strong>Tempo de percurso</strong>: 3 minutos. <br>
+                <strong>Nível de dificuldade de acesso</strong>: muito fácil. <br>
+            </div>
         </div>
     </div>
 </section>
