6968fa1944ef95fd4a3e79e1730e52af19734b4c
erika.fidencio <henriquejgs@gmail.com>
2016-10-07 14:09:39 -0300

    atrativos em andamento

--- application/views/site/atrativos.php
+++ application/views/site/atrativos.php
@@ -8,18 +8,18 @@
 <section class="atrativos">
     <h2>Cachoeiras</h2>
     <div class="container">
-        <div >
+        <div>
             <h3>Cachoeira das Andorinhas</h3>
             <figure>
                 <img src="<? echo base_url() . IMG_ATRATIVOS ?>cachoeiraandorinhas.jpg" alt="cachoeira das andorinhas">
             </figure>
-            <div >
+            <div>
                 A Cachoeira das Andorinhas possui uma beleza natural singular, conta com uma queda d'água de cerca de 10
                 metros de altura, situada no interior de uma formação rochosoa que se assemelha a uma gruta. É assim
                 chamada
                 por abrigar uma considerável quantidade de andorinhas de coleira durante a primavera-verão.
             </div>
-            <div >
+            <div>
                 <strong>Distância da sede do parque</strong>: 550m.<br>
                 <strong>Tempo de percurso</strong>: 20 minutos. <br>
                 <strong>Nível de dificuldade de acesso</strong>: normal. <br>
@@ -30,14 +30,14 @@
             <figure>
                 <img src="<? echo base_url() . IMG_ATRATIVOS ?>cachoeiradospelados.jpg" alt="cachoeira dos pelados">
             </figure>
-            <div >
+            <div>
                 A Cachoeira dos Pelados situa-se a cerca de 600 metros da sede do parque. Esta cachoeira, de beleza
                 cênica
                 exuberante, conta com uma queda d'água de aproximadamente 5 metros de altura, perfeita para se refrescar
                 em
                 ambiente tranquilo e harmonioso.
             </div>
-            <div >
+            <div>
                 <strong>Distância da sede do parque</strong>: 600 m.<br>
                 <strong>Tempo de percurso</strong>: 40 minutos. <br>
                 <strong>Nível de dificuldade de acesso</strong>: dificil. <br>
@@ -48,13 +48,13 @@
             <figure>
                 <img src="<? echo base_url() . IMG_ATRATIVOS ?>cachoeiraveudanoiva.jpg" alt="cachoeira Veu das Noivas">
             </figure>
-            <div >
+            <div>
                 A Cachoeira Véu das Noivas apresenta duas quedas d'água que somam cerca de 40 metros de altura de
                 deslumbrante e imponente beleza natural. Chegar a base desta cachoeira é tarefa para os visitantes mais
                 aventureiros, pois é necessário atravessar uma trilha bem fechada e muito íngrime que liga o topo à base
                 desta cachoeira.
             </div>
-            <div >
+            <div>
                 <strong>Distância da sede do parque</strong>: 1,10 m.<br>
                 <strong>Tempo de percurso</strong>: 60 minutos. <br>
                 <strong>Nível de dificuldade de acesso</strong>: muito dificil. <br>
@@ -65,7 +65,7 @@
             <figure>
                 <img src="<? echo base_url() . IMG_ATRATIVOS ?>cachoeiradafolhinha.jpg" alt="cachoeirada Folhinha">
             </figure>
-            <div >
+            <div>
                 A Cachoeira da Folhinha é um dos pontos mais procurados pelos visitantes que moram na região. Este
                 atrativo
                 turístico natural de beleza magnífica, conta com um grande poço que se assemelha a um belo lago, ideal
@@ -74,7 +74,7 @@
                 pequenas
                 folhas que margeam o local, engrandecendo ainda mais seu fascinante encanto natural.
             </div>
-            <div >
+            <div>
                 <strong>Distância da sede do parque</strong>: 2,45 km.<br>
                 <strong> Tempo de percurso</strong>: 75 minutos. <br>
                 <strong>Nível de dificuldade de acesso</strong>: muito dificil. <br>
@@ -85,7 +85,7 @@
             <figure>
                 <img src="<? echo base_url() . IMG_ATRATIVOS ?>praiadorancheiro.jpg" alt="Praia do Rancheiro ">
             </figure>
-            <div >
+            <div>
                 A Praia do Rancheiro é um excelente local para nadar e revigorar as energias em boa companhia. Contêm um
                 grande poço de pouco mais de 1,5 metros de profundidade, além de uma extensa área plana coberta por
                 gramínias. Ótimo local para se acampar e fazer piquiniques com amigos e família. Lembrando que não é
@@ -93,7 +93,7 @@
                 própria
                 segurança.
             </div>
-            <div >
+            <div>
                 <strong>Distância da sede do parque</strong>: 3,0 km.<br>
                 <strong>Tempo de percurso</strong>: 100 minutos. <br>
                 <strong>Nível de dificuldade de acesso</strong>: muito dificil. <br>
@@ -105,14 +105,14 @@
                 <img src="<? echo base_url() . IMG_ATRATIVOS ?>cachoeiradocarioquinha.jpg"
                      alt="Cachoeira do Carioquinha">
             </figure>
-            <div >
+            <div>
                 A Cachoeira do Carioquinha é de extrema beleza cênica, contendo uma pequena e refrescante queda d'água,
                 além
                 de um extenso poço, onde a profundidade aumenta progressivamente, chegando até cerca de 1,5 metros de
                 profundidade próximo da queda d'água. É ideal para visitantes que tem receio de nadar em cachoeiras mais
                 profundas e com grandes quedas d'água.
             </div>
-            <div >
+            <div>
                 <strong>Distância da sede do parque</strong>: 1,54 km.<br>
                 <strong>Tempo de percurso</strong>: 60 minutos. <br>
                 <strong>Nível de dificuldade de acesso</strong>: normal. <br>
@@ -123,7 +123,7 @@
             <figure>
                 <img src="<? echo base_url() . IMG_ATRATIVOS ?>pocinhodobau.jpg" alt="Pocinho do Baú">
             </figure>
-            <div >
+            <div>
                 O Pocinho do Baú é ideal para visitantes que não desejam caminhar longos percursos para desfrutar de uma
                 área para nadar com espetacular beleza natural. Ese poço apresenta cerca de 1,80 metros de profundidade,
                 em
@@ -132,7 +132,7 @@
                 do
                 parque.
             </div>
-            <div >
+            <div>
                 <strong>Distância da sede do parque</strong>: 690 m.<br>
                 <strong>Tempo de percurso</strong>: 15 minutos. <br>
                 <strong>Nível de dificuldade de acesso</strong>: fácil. <br>
@@ -144,19 +144,124 @@
                 <img src="<? echo base_url() . IMG_ATRATIVOS ?>pocinhodaschurrasqueiras.jpg"
                      alt="Pocinho das Churrasqueiras">
             </figure>
-            <div >
+            <div>
                 O Pocinho das Churrasqueiras é de extremo encanto natural que se encontra próximo a área de
                 churrasqueiras.
                 Poço de cerca de 1,0 metros de profundidade, ótimo para se refrescar com as crianças, família e amigos
                 enquanto prepara um saboroso churrasco nos dias ensolarados.
             </div>
-            <div >
-                <strong>Distância da sede do parque</strong>: 170 m.<br>
+            <div>
+                <strong>Distância da sede do parque</strong>: 170 metros.<br>
                 <strong>Tempo de percurso</strong>: 3 minutos. <br>
                 <strong>Nível de dificuldade de acesso</strong>: muito fácil. <br>
             </div>
         </div>
+        <h2>Mirantes Naturais</h2>
+        <div>
+            <h3>Pedra do Jacaré</h3>
+            <figure>
+                <img src="<? echo base_url() . IMG_ATRATIVOS ?>pedradojacare.jpg" alt="Pedra do Jacaré">
+            </figure>
+            <div>
+                A Pedra do Jacaré é uma formação rochosa de coloração esbranquiçada com um curioso formato de uma cabeça
+                de jacaré. Neste mirante é possível observar também uma deslumbrante paisagem natural composta por
+                serras e mata atlântica que serve como plano de fundo para a famosa e belíssima Pedra do Jacaré.
+            </div>
+            <div>
+                <strong>Distância da sede do parque</strong>: 525 metros.<br>
+                <strong>Tempo de percurso</strong>: 20 minutos. <br>
+                <strong>Nível de dificuldade de acesso</strong>: fácil. <br>
+            </div>
+        </div>
+        <div>
+            <h3>Cachoeira Véu das Noivas</h3>
+            <figure>
+                <img src="<? echo base_url() . IMG_ATRATIVOS ?>miranteveudasnoivas.jpg" alt="Cachoeira Véu das Noivas">
+            </figure>
+            <div>
+                Este mirante natural trata-se do topo da Cachoeira Véu das Noivas, de onde é possível contemplar a
+                beleza majestosa da natureza com uma exuberante mata atlântica e serras de até 1.200 metros de altitude.
+            </div>
+            <div>
+                <strong>Distância da sede do parque</strong>: 820 metros.<br>
+                <strong>Tempo de percurso</strong>: 40 minutos. <br>
+                <strong>Nível de dificuldade de acesso</strong>: difícil. <br>
+            </div>
+        </div>
+        <div>
+            <h3>Pedra Branca</h3>
+            <figure>
+                <img src="<? echo base_url() . IMG_ATRATIVOS ?>pedrabranca.jpg" alt="Pedra Branca">
+            </figure>
+            <div>
+                O mirante natural da Pedra Branca se situa no topo de uma formação rochosa de coloração esbranquiçada,
+                daí a origem do nome. Trata-se do mirante mais alto do parque, com cerca de 1.350 metros de altitude. Do
+                topo da Pedra Branca é possível observar quase toda a extensão do parque e contemplar uma paisagem de
+                beleza singular. Os mais aventureiros costumam chegar ao mirante através da escalada da Pedra Branca.
+                Mas se você não quer se arriscar tanto, não se preocupe! Existe uma trilha de nível moderado que dá
+                acesso ao topo da pedra.
+            </div>
+            <div>
+                <strong>Distância da sede do parque</strong>: 940 metros.<br>
+                <strong>Tempo de percurso</strong>: 45 minutos. <br>
+                <strong>Nível de dificuldade de acesso</strong>: normal. <br>
+            </div>
+        </div>
+        <h2>Setores de Escalada</h2>
+        <div>
+            <h3>Pedra do Arco</h3>
+            <figure>
+                <img src="<? echo base_url() . IMG_ATRATIVOS ?>pedradoarco.jpg" alt="Pedra do Arco">
+            </figure>
+            <div>
+                A Pedra do Arco é um setor de escalada natural ao ar livre com cerca de 12 metros de altura em paredão
+                vertical contendo vias ativas já instaladas no local. A formação rochosa trata-se de quartzito compacto
+                e em ótimas condições para a prática deste esporte.
+            </div>
+            <div>
+                <strong>Distância da sede do parque</strong>: 940 metros.<br>
+                <strong>Tempo de percurso</strong>: 45 minutos. <br>
+                <strong>Nível de dificuldade de acesso</strong>: difícil. <br>
+            </div>
+        </div>
+        <div>
+            <h3>Pedra Branca</h3>
+            <figure>
+                <img src="<? echo base_url() . IMG_ATRATIVOS ?>pedra.jpg" alt="Pedra do Arco">
+            </figure>
+            <div>
+                O setor de escalada Pedra Branca conta com mais de um paredão para a prática do esporte. A modalidade de
+                escalada neste local é em parede vertical, onde já existem algumas vias com diferentes níveis de
+                dificuldade. No topo deste setor se encontra o mirante natural mais alto do parque, de onde é possível
+                observar uma paisagem natural de extrema beleza cênica.
+            </div>
+            <div>
+                <strong>Distância da sede do parque</strong>: 820 metros.<br>
+                <strong>Tempo de percurso</strong>: 40 minutos. <br>
+                <strong>Nível de dificuldade de acesso</strong>: normal. <br>
+            </div>
+        </div>
+        <div>
+            <h3>Boulder Bonsai</h3>
+            <figure>
+                <img src="<? echo base_url() . IMG_ATRATIVOS ?>boulderbonsai.jpg" alt="Boulder Bonsai">
+            </figure>
+            <div>
+                O setor de escalada Bonsai contém apenas um local para escalada, no entanto, existe mais de uma via para
+                a prática deste esporte. Este setor de escalada é do tipo boulder, modalidade onde se escala uma parede
+                com inclinação negativa (teto), além disso, este setor dispensa o uso de cordas e ganchos fixos na
+                rocha.
+            </div>
+            <div>
+                <strong>Distância da sede do parque</strong>: 1,80 Km.<br>
+                <strong>Tempo de percurso</strong>: 80 minutos. <br>
+                <strong>Nível de dificuldade de acesso</strong>: normal. <br>
+            </div>
+        </div>
+
     </div>
+
+
 </section>
 
 
