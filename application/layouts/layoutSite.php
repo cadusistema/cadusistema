c2c356cd6f35773e756bdb39a42533b55acbc11a
erika.fidencio <henriquejgs@gmail.com>
2016-11-09 12:15:09 -0200

    site com elementos maiores que o body

--- application/layouts/layoutSite.php
+++ application/layouts/layoutSite.php
@@ -65,36 +65,39 @@
 
 </head>
 <body>
-<nav class="navbar navbar-default navbar navbar-fixed-top   ">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-        </div>
+<section id="menu">
+    <nav class="navbar navbar-default navbar navbar-fixed-top   ">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav nav-justified">
-                <li><a href="#"><img src="<? echo base_url(ICONE . 'logo_barra.png') ?>" height="30px" alt=""><b> Parque
-                            das Andorinhas</b></a></li>
-                <li><a href="#">Infra-Estrutura <span class="sr-only">(current)</span></a></li>
-                <li><a href="#">Esportes</a></li>
-                <li><a href="#">Atividades Desenvolvidas</a></li>
-                <li><a href="#">Atrativos Turísticos</a></li>
-                <li><a href="#">Localização</a></li>
-                <li><a href="#">Contato</a></li>
-                <li><a href="#">Entrar</a></li>
-            </ul>
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-</nav>
-<section>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav nav-justified">
+                    <li><a href="#"><img src="<? echo base_url(ICONE . 'logo_barra.png') ?>" height="30px" alt=""><b>
+                                Parque
+                                das Andorinhas</b></a></li>
+                    <li><a href="#">Infra-Estrutura <span class="sr-only">(current)</span></a></li>
+                    <li><a href="#">Esportes</a></li>
+                    <li><a href="#">Atividades Desenvolvidas</a></li>
+                    <li><a href="#">Atrativos Turísticos</a></li>
+                    <li><a href="#">Localização</a></li>
+                    <li><a href="#">Contato</a></li>
+                    <li><a href="#">Entrar</a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+</section>
+<section id="carousel">
     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
         <!-- Indicators -->
         <ol class="carousel-indicators">
